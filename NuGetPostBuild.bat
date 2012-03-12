f1e1af73089376c0ef6546c4ad9f522a165def6f
Jim Swainston <jswainston@sportingindex.com>
2012-03-12 15:13:58 +0000

    Changed way we use NuGet

--- NuGetPostBuild.bat
+++
@@ -1,3 +0,0 @@
-REM Calls NuGet
-echo on
-CALL "%~1..\NuGet.exe" pack "%~2"  -OutputDirectory "%~1.."
--- NuGetPreBuild.bat
+++
@@ -1,2 +0,0 @@
-REM Calls NuGet
-CALL "%~1..\NuGet.exe" install "%~1packages.config" -o "%~1..\Packages"
