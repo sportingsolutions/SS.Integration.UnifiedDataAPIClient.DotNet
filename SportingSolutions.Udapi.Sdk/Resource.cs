a4693ada999baf6e611e356373ff4f29a8e28a18
Jedrzej Sztompka <jsztompka@sportingindex.com>
2012-04-17 10:37:10 +0100

    Added version to relations

--- SportingSolutions.Udapi.Sdk/Resource.cs
+++ SportingSolutions.Udapi.Sdk/Resource.cs
@@ -61,7 +61,7 @@ namespace SportingSolutions.Udapi.Sdk
         {
             if (State != null)
             {
-                var theLink = State.Links.First(restLink => restLink.Relation == "http://api.sportingsolutions.com/rels/snapshot");
+                var theLink = State.Links.First(restLink => restLink.Relation == "http://api.sportingsolutions.com/v001/rels/snapshot");
 
                 return RestHelper.GetResponse(new Uri(theLink.Href), null, "GET", "application/json", Headers);
             }
@@ -79,7 +79,7 @@ namespace SportingSolutions.Udapi.Sdk
 
         private void StreamData()
         {
-            var restItems = FindRelationAndFollow("http://api.sportingsolutions.com/rels/stream/amqp");
+            var restItems = FindRelationAndFollow("http://api.sportingsolutions.com/v001/rels/stream/amqp");
             var amqpLink = restItems.SelectMany(restItem => restItem.Links).First(restLink => restLink.Relation == "amqp");
 
             var amqpUri = new Uri(amqpLink.Href);
