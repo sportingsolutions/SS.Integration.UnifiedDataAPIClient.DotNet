dd150051d4fdf9d6edf897aabf32d88551b2407b
Jim Swainston <jswainston@sportingindex.com>
2012-05-11 10:31:47 +0100

    Added match status to summary

--- SportingSolutions.Udapi.Sdk/Model/Summary.cs
+++ SportingSolutions.Udapi.Sdk/Model/Summary.cs
@@ -20,18 +20,10 @@ namespace SportingSolutions.Udapi.Sdk.Model
     {
         public string Id { get; set; }
 
-        public string Description { get; set; }
-
-        public List<Participant> Participants { get; set; }
-
         public string Date { get; set; }
 
         public List<Tag> Tags { get; set; }
 
-        public string DefinitionId { get; set; }
-
-        public string DefinitionName { get; set; }
-
-        public string Type { get; set; }
+        public int MatchStatus { get; set; }
     }
 }
