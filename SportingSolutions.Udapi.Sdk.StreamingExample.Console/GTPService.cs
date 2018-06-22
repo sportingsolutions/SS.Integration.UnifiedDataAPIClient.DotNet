0b5560212e574b8d1b233d97b18c0b98e47d7822
Sergey Filipov <sergey.filipov@1pt.com>
2018-06-22 10:33:30 +0300

    sports added to console streaming

--- SportingSolutions.Udapi.Sdk.StreamingExample.Console/GTPService.cs
+++ SportingSolutions.Udapi.Sdk.StreamingExample.Console/GTPService.cs
@@ -39,7 +39,7 @@ namespace SportingSolutions.Udapi.Sdk.StreamingExample.Console
         {
             _settings = settings ?? Settings.Instance;
             _logger = LogManager.GetLogger(typeof(GTPService).ToString());
-            _sportsList = new List<string> {"Football"};
+            _sportsList = new List<string> { "Football", "Basketball", "IceHockey", "Tennis", "RugbyUnion", "TennisDoubles", "Volleyball" };
             _listeners = new ConcurrentDictionary<string, StreamListener>();
             _activeFixtures = new ConcurrentDictionary<string, bool>();
         }
